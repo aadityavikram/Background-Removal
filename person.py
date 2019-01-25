5ace13fc37cf25231f0bb0d65290ae2087d4a049
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2019-01-25 15:45:27 +0530

    Update person.py

--- person.py
+++ person.py
@@ -28,26 +28,19 @@ def main():
     print("Removing background...")
     print()
     image = Image.open(path)
-    resized_image = imresize(image, (224, 224)) / 255.0
+    image1 = imresize(image, (224, 224)) / 255.0
 
-    # Model input shape = (224,224,3)
-    # [0:3] - Take only the first 3 RGB channels and drop ALPHA 4th channel in case this is a PNG
-    prediction = predict(resized_image[:, :, 0:3])
+    prediction = predict(image1[:, :, 0:3])
     print('##################################################################')
 
-    # Resize back to original image size
-    # [:, :, 1] = Take predicted class 1 - currently in our model = Person class. Class 0 = Background
     prediction = imresize(prediction[:, :, 1], (image.height, image.width))
     prediction[prediction>0.5*255] = 255
     prediction[prediction<0.5*255] = 0
 
-    # Append transparency 4th channel to the 3 RGB image channels.
-    transparent_image = np.append(np.array(image)[:, :, 0:3], prediction[: , :, None], axis=-1)
-    transparent_image = Image.fromarray(transparent_image)
+    transparency = np.append(np.array(image)[:, :, 0:3], prediction[: , :, None], axis=-1)
+    png = Image.fromarray(transparency)
 
-    new_image = transparent_image
-
-    new_image.save('output.png')
+    png.save('output.png')
 
     print()
     print("saved the output image in output.png")
